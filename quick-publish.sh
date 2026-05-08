a4874a18a1b8b3cf8ed3ca6b50cf7afbb5f42dcd
Stefan Zeiger <stefan.zeiger@databricks.com>
2026-05-08 16:38:17 +0200

    Update patch level to db-3

--- quick-publish.sh
+++ quick-publish.sh
@@ -5,4 +5,4 @@ set -eo pipefail
 sbt zincCore2_13/packageBin
 
 cp internal/zinc-core/target/jvm-2.13/zinc-core_2.13-1.10.6-SNAPSHOT.jar \
-  ../universe3/maven/scala-worker/org.scala-sbt/zinc-core_2.13/org.scala-sbt__zinc-core_2.13__1.10.8-bin-db-1-876b1ad98.jar
+  ../universe2/maven/scala-worker/org.scala-sbt/zinc-core_2.13/org.scala-sbt__zinc-core_2.13__1.10.8-bin-db-2-b0cf56f04.jar
